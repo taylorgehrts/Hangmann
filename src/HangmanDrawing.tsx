df116265bc490b5d0e23bf5893a87f3f83af1fc5
taylorgehrts <taylorgehrts@gmail.com>
2023-12-19 13:29:55 +0100

    added word list and components for gallos

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,10 @@
+export function HangmanDrawing() {
+    return <div style={{position: "relative"}}>
+        <div style={{height: "50px", width: "10px", background: "black", position:"absolute",  top: 0, right: 0}} />
+        <div style={{height: "10px", width: "200px", background: "black", marginLeft:"120px"}} />
+        <div style={{height: "400px", width: "10px", background: "black", marginLeft:"120px"}} />
+        <div style={{height: "10px", width: "250px", background: "black"}} />
+
+    </div>
+    
+}
