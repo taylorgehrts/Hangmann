761da2f0c99d5bbec7ab85e023d61cd72cc36299
taylorgehrts <taylorgehrts@gmail.com>
2023-12-20 11:38:22 +0100

    added keyboard element

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,10 @@ function App() {
     <div style={{ fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
     <HangmanDrawing />
     <HangmanWord />
+    <div style={{ alignSelf: "stretch"}}>
     <Keyboard />
+    </div>
+   
 
   </div>
 
